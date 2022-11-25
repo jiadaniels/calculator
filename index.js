519bc9ed33c98efc7f1557eb1182a75ac9f48f73
jiadaniels <jiadaniels77@gmail.com>
2022-11-25 16:14:19 +0200

    calculator

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 let display = document.getElementById('display');
-let output = document.getElementById('output');
-let buttons = Array.from(document.getElementsByTagName("button"));
+let input = document.getElementById('input');
+let buttons = Array.from(document.getElementsByTagName('button'));
+let multiply = document.getElementById('multiply');
 
 buttons.map(button => {
     button.addEventListener('click' , (e) => {
         switch(e.target.innerText){
             case 'AC':
-                output.innerText = '';
+                input.innerText = '';
                 display.innerText = '';
                 break;
             case 'Del':
-                output.innerText = output.innerText.slice(0, -1);
+                input.innerText = input.innerText.slice(0, -1);
                 display.innerText = '';
                 break;
             case '=':
                 try{
-                   display.innerText = eval(output.innerText);
+                   display.innerText = eval(input.innerText);
+                   console.log(input.innerText)
                 } catch {
-                    output.innerText = 'Error';
+                    input.innerText = 'Error';
                 }
                 break;
-            // case 'x':
-            //     if(display.innerText){
-            //             display.innerText = '*'
-            //     }
-            //     break;
-
             default:
-                output.innerText += e.target.innerText;
+                input.innerText += e.target.innerText;
         }
     });
 });
